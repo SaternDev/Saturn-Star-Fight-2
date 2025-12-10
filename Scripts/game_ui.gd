2752e3da5774e83cdbb98056bcc00a20f16d9133
SaternDev <saterndev@gmail.com>
2025-12-10 15:54:17 +0100

    feat(ui): update HUD and add new menu interface
    - Modify lives and points display
    - Implement new menu system
    - Enhance user interface elements

--- Scripts/game_ui.gd
+++ Scripts/game_ui.gd
@@ -1,15 +1,25 @@
 extends CanvasLayer
 
-@onready var pointsLabel: Label = $GameUI/PointsAndLives/Points
+@onready var points_label: Label = $GameUI/Points/PointsLabel
 @onready var difficultity: Label = $GameUI/Difficultity/Difficultity
-@onready var lives: Label = $GameUI/PointsAndLives/Lives
+@onready var health_img: TextureRect = $GameUI/Lives/HealthImg
+
+const HEAL_BAR_SHIP_2 = preload("uid://dtd73qqtvw77m")
+const HEAL_BAR_SHIP_3 = preload("uid://dh328kf4nfkg1")
+const HEAL_BAR_SHIP_4 = preload("uid://qvpcil8hm5cl")
+
 
 func _on_game_controller_pointschanged(score) -> void:
-	pointsLabel.text = str("Points: ", score)
+	points_label.text = str("Points: ", score)
 
 func _on_game_controller_difficult_change(difficult) -> void:
 	difficultity.text = str(difficult)
 
 
 func _on_game_controller_hp_change(live) -> void:
-	lives.text = str("Lives: ", live)
+	if  live == 2:
+		health_img.texture = HEAL_BAR_SHIP_2
+	elif live == 1:
+		health_img.texture = HEAL_BAR_SHIP_3
+	else:
+		health_img.texture = HEAL_BAR_SHIP_4
