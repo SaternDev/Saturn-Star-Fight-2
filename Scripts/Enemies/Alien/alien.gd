efa9dee0ef485f1ce345a894f73ae2bbe2440c3f
SaternDev <saterndev@gmail.com>
2026-08-19 21:46:50 +0200

    Fixed Alien Shooting
    Added the posibility to the alien to shoot

--- Scripts/Enemies/Alien/alien.gd
+++ Scripts/Enemies/Alien/alien.gd
@@ -41,4 +41,3 @@ func _on_area_entered(area: Area2D) -> void:
 	elif area is Bullet:
 		area.queue_free()
 		life -= 1
-		
--- Scripts/Enemies/Alien/bullet_alien.gd
+++ Scripts/Enemies/Alien/bullet_alien.gd
@@ -4,9 +4,9 @@ extends Area2D
 @export var speed = 350.0
 
 func _process(delta: float) -> void:
-	position.y -= delta * speed
+	position.y += delta * speed
 	
-	if global_position.y < 610:
+	if global_position.y > 610:
 		Destroy()
 
 func Destroy():
@@ -16,3 +16,4 @@ func Destroy():
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Players"):
 		body.playerHit()
+		Destroy()
